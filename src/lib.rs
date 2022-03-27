a0cdbdd796750abfd1627313984756219f59f19b
Emil Privér <emil@rivercode.se>
2022-03-27 22:19:32 +0200

    dev

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use worker::*;
+use std::collections::HashMap;
 
 mod utils;
+mod image_resize;
 
 fn log_request(req: &Request) {
     console_log!(
@@ -23,12 +25,18 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     router
         .get("/", |req, ctx| {
             let request_url = req.url().unwrap_or_else(|err| panic!("Could not parse '{:?}': {}", stringify!($var), err));
-            let url = Url::parse(request_url.as_str())?;
-            let query = url.query();
-            
-            console_log!("{:?}" ,query);
+            let parsed_url = Url::parse(request_url.as_str())?;
+            let hash_query: HashMap<_, _> = parsed_url.query_pairs().into_owned().collect();
 
-            Response::ok(req.url()?.to_string())
+            let image_src = hash_query.get("src").unwrap();
+            let image_width = hash_query.get("w").unwrap();
+            let image_quality = hash_query.get("q").unwrap();
+            
+            console_log!("{:?}" ,image_src);
+            console_log!("{:?}" ,image_width);
+            console_log!("{:?}" ,image_quality);
+
+            Response::ok(hash_query.get("src").unwrap())
         })
         .get("/worker-version", |_, ctx| {
             let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
